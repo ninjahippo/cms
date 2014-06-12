1d10d42170743de7f21806a1888f41ae288e1dcd
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-12 11:51:10 -0500

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinjahippoCms::Application.config.secret_token = '66e487bd48062e3e9b6cd545fcc722148c97e6bde56e8c6a0f81bef249eaebdc0b92c5d793d0301a54f1aa032379c2b7c2a0736eb7b70ca63eda7ce179491d8a'
