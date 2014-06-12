2704972879985535671c0a992f774ce545e40289
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-12 18:20:53 -0500

    Changing 'show' functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,7 +28,7 @@ class PagesController < ApplicationController
 
   def show
     respond_to do |format|
-      format.html { @page = current_user.sites.find_by_slug(params[:site_id]).pages.find_by_slug(params[:id]) }
+      format.html { redirect_to page.site.url + page.slug }
       format.json { 
         if Site.find_by_application_token(params[:application_token]) and Site.find_by_application_token(params[:application_token]).pages.where(:slug => params[:id]).any?
           render json: Site.find_by_application_token(params[:application_token]).pages.where(:slug => params[:id])
