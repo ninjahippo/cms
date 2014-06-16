72784a05c377851adc964ea45215f74b0ce697c4
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-15 21:47:39 -0500

    bug fix

--- public/scripts/controllers/sites/new.js
+++ public/scripts/controllers/sites/new.js
@@ -2,7 +2,7 @@
 
 angular.module('NinjahippoCMS').controller('NewSiteCtrl', function ($scope, Restangular, $filter, Auth, $location, $rootScope) {
   $scope.create_site = function(site) {
-    Restangular.post('sites', site).then(function(){
+    Restangular.all('sites').post(site).then(function(){
       $location.path('/dashboard')
     })
   }
