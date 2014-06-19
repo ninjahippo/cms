8dfeb6d82c56f63fff0a344162b929daf67f9b76
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-18 20:20:01 -0500

    removing old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 NinjahippoCms::Application.routes.draw do
   devise_for :users
 
-  scope '/api', defaults: { format: 'json' } do
-    scope '/v1' do
-      resources :sites do
-        resources :pages
-      end
-    end
-  end
-
   get '/auth_token' => 'static#auth_token'
 
   match '*path' => 'static#index'
