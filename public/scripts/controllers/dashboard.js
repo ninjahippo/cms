fea8a61f62f319f4a7e588503e764808a07866ee
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-22 09:26:34 -0500

    beautified dashboard a bit more by limiting the length of the shown title, slug etc

--- public/scripts/controllers/dashboard.js
+++ public/scripts/controllers/dashboard.js
@@ -6,10 +6,19 @@ angular.module('NinjahippoCMS').controller('DashboardCtrl', function (api, $scop
     Restangular.all('sites').getList({api_token: d.token}).then(function(sites) {
       $scope.sites = sites;
       for (var i = 0; i < $scope.sites.length; i++) {
+        var site = $scope.sites[i];
+        site.shown_slug = site.slug;
+        if (site.title.length > 21) {
+          site.title = site.title.substr(0, 18) + '...';
+        }
+        if (site.slug.length > 21) {
+          site.shown_slug = site.slug.substr(0,18) + '...';
+        }
+
         $scope.sites[i].pages = [];
         Restangular.one('sites', $scope.sites[i].slug).getList('pages', {api_token: d.token}).then(function(pages){
           for (var i = 0; i < pages.length; i++) {
-            $scope.sites[i].pages.push(pages[i])
+            $scope.sites[i].pages.push(pages[i]);
           }
         });
       }
--- public/scripts/controllers/sites/show.js
+++ public/scripts/controllers/sites/show.js
@@ -6,6 +6,17 @@ angular.module('NinjahippoCMS').controller('ShowSiteCtrl', function (api, $scope
       $scope.site = site;
       Restangular.one('sites', $routeParams.slug).getList('pages', {api_token: d.token}).then(function(pages){
         $scope.site.pages = pages;
+
+        for (var i = 0; i < $scope.site.pages.length; i++) {
+          var page = $scope.site.pages[i];
+          page.shown_slug = page.slug;
+          if (page.title.length > 21) {
+            page.title = page.title.substr(0, 18) + '...';
+          }
+          if (page.slug.length > 21) {
+            page.shown_slug = page.slug.substr(0,18) + '...';
+          }
+        }
       })
     });
   });
