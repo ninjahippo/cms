85e7602bf3e421c818e26cc6293e4cf9e0b3ed7c
Joah Gerstenberg <joahd@gbergfamily.com>
2014-07-02 18:28:31 -0500

    add auth_token authentication stuff

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,13 @@ class StaticController < ApplicationController
   end
 
   def auth_token
+    if params[:email] and params[:password]
+      @user = User.find_by_email(params[:email])
+      if @user.valid_password?(params[:password])
+        sign_in @user
+      end      
+    end
+
     respond_to do |format|
       if current_user
         format.json { render json: { :status => 'ok', :token => current_user.authentication_token } }
